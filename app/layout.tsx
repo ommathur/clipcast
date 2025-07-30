ed0ee6065447208dde6c17acd841a9ff5a307a7b
ommathur <ommathur2404@gmail.com>
2025-07-30 17:49:58 +0530

    Changing icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ClipCast",
   description: "Quickly share text/files via QR",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
